da9173c9474dbc4db782e55427623254aa6e2eef
Murilo Freire <murilo.freire@volvocars.com>
2023-07-13 06:37:57 +0200

    adding router

--- src/renderer/appRenderer.tsx
+++ src/renderer/appRenderer.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Application from './components/Application';
-
+import Index from "./components/Index"
 // Say something
 console.log('[ERWT] : Renderer execution started');
 
 // Render application in DOM
-createRoot(document.getElementById('app')).render(<Application />);
+createRoot(document.getElementById('app')).render(<BrowserRouter><Routes>
+    <Route path="/Index" element={<Index />} />
+    <Route path="*" element={<Application />} />
+  </Routes>
+</BrowserRouter>);
--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -1,50 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import './Application.scss';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
 import { icons } from './Icons';
 
+
 const Application: React.FC = () => {
-  const [counter, setCounter] = useState(0);
-  const [darkTheme, setDarkTheme] = useState(true);
   const [versions, setVersions] = useState<Record<string, string>>({});
 
-  /**
-   * On component mount
-   */
-  useEffect(() => {
-    const useDarkTheme = parseInt(localStorage.getItem('dark-mode'));
-    if (isNaN(useDarkTheme)) {
-      setDarkTheme(true);
-    } else if (useDarkTheme == 1) {
-      setDarkTheme(true);
-    } else if (useDarkTheme == 0) {
-      setDarkTheme(false);
-    }
 
-    // Apply verisons
+  useEffect(() => {
     const app = document.getElementById('app');
     const versions = JSON.parse(app.getAttribute('data-versions'));
     setVersions(versions);
   }, []);
-
-  /**
-   * On Dark theme change
-   */
-  useEffect(() => {
-    if (darkTheme) {
-      localStorage.setItem('dark-mode', '1');
-      document.body.classList.add('dark-mode');
-    } else {
-      localStorage.setItem('dark-mode', '0');
-      document.body.classList.remove('dark-mode');
-    }
-  }, [darkTheme]);
-
-  /**
-   * Toggle Theme
-   */
-  function toggleTheme() {
-    setDarkTheme(!darkTheme);
-  }
 
   return (
     <div id='erwt'>
@@ -113,32 +81,7 @@ const Application: React.FC = () => {
             </div>
             <span>{versions?.license}</span>
           </div>
-        </div>
-      </div>
-
-      <div className='footer'>
-        <div className='center'>
-          <button
-            onClick={() => {
-              if (counter > 99) return alert('Going too high!!');
-              setCounter(counter + 1);
-            }}
-          >
-            Increment {counter != 0 ? counter : ''} <span>{counter}</span>
-          </button>
-          &nbsp;&nbsp; &nbsp;&nbsp;
-          <button
-            onClick={() => {
-              if (counter == 0) return alert('Oops.. thats not possible!');
-              setCounter(counter > 0 ? counter - 1 : 0);
-            }}
-          >
-            Decrement <span>{counter}</span>
-          </button>
-          &nbsp;&nbsp; &nbsp;&nbsp;
-          <button onClick={toggleTheme}>
-            {darkTheme ? 'Light Theme' : 'Dark Theme'}
-          </button>
+          <Link to="/Index">Nothing Here</Link>
         </div>
       </div>
     </div>
